2f1d5c0183bb9160a0b0e49bdd773b9ac293baa6
Sergey Shandar <sergey@live.com.au>
2017-05-24 23:30:34 -0700

    version

--- build.ps1
+++ build.ps1
@@ -3,4 +3,10 @@ $nugetExe = Join-Path (pwd) "nuget.exe"
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://dist.nuget.org/win-x86-commandline/v4.1.0/nuget.exe", $nugetExe)
 
-.\nuget.exe pack DagVer.nuspec
+$version = if ($env:APPVEYOR_BUILD_NUMBER) {
+    "0.0.0." + $env:APPVEYOR_BUILD_NUMBER
+} else {
+    "0-unknown"
+}
+
+.\nuget.exe pack DagVer.nuspec -Version $version
